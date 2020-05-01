8d1712d3d4d3b992f6adbbba68fa1b172952a4a0
X-Track <xxttrack@gmail.com>
2020-05-01 13:33:37 +0200

    Build 0.6.01
    Test versie vrijdag

---
+++ Prototype_team_project/Assets/Scripts/rotatecamerascript1up.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatecamerascript1up : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.Rotate(speed * Time.deltaTime,0,0);
+    }
+}
