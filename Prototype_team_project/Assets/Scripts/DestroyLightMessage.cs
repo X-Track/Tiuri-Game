6de2bb5334eebead2a01bf1e03c6f5d20270bf49
X-Track <xxttrack@gmail.com>
2020-05-01 14:50:28 +0200

    Build 0.6.01
    fixed: darkroom message, parkour exit, dialog, sound on grass & added: titlescreen

---
+++ Prototype_team_project/Assets/Scripts/DestroyLightMessage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyLightMessage : MonoBehaviour
+{
+
+    public GameObject lightMessage;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(lightMessage);
+        }
+    }
+}
